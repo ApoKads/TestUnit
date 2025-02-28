436722e7411283100f300212a004dfc2de7da4da
theoraas07 <theoraalpha07@gmail.com>
2025-02-28 15:00:04 +0700

    push beneran

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,13 @@
 import unittest
-from  app import app
+from src.app import app
 
 class TestApp(unittest.TestCase):
-    def test_home(self):
-        response = app.test_client().get('/')
-        self.assertEqual(response.data,b'Hello, World!')
 
-if __name__ == "__main__":
-    unittest.main()
+    def test_home(self):
+        with app.test_client() as client: #use the imported app variable.
+            response = client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == '__main__':
+   unittest.main()
